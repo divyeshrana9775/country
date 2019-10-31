9726f71b39e983b4534e4c95fe45b595654fb68d
divyeshrana9775 <divyeshrana9775@gmail.com>
2019-10-30 23:09:14 -0400

    Updates

---
+++ precache-manifest.42cab2d4e71ae5b4e8dda52bf20a9879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a2b119fc67fd5934de6016106823af",
+    "url": "/country/index.html"
+  },
+  {
+    "revision": "32195c014728e89f4952",
+    "url": "/country/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d52974d25ab933aab39",
+    "url": "/country/static/js/2.f5cf7325.chunk.js"
+  },
+  {
+    "revision": "32195c014728e89f4952",
+    "url": "/country/static/js/main.0efd5e60.chunk.js"
+  },
+  {
+    "revision": "f3de19e18e01f0295402",
+    "url": "/country/static/js/runtime-main.fb6ef1a1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/country/static/media/logo.25bf045c.svg"
+  }
+]);
